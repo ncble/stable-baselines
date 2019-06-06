4f1cf2617ee8443541fd8f422c83f5bea706c4fc
lulin <ncble17@gmail.com>
2019-06-06 15:04:24 +0200

    test gpu session

--- stable_baselines/common/tf_util.py
+++ stable_baselines/common/tf_util.py
@@ -95,10 +95,11 @@ def make_session(num_cpu=None, make_default=False, graph=None):
     """
     if num_cpu is None:
         num_cpu = int(os.getenv('RCALL_NUM_CPU', multiprocessing.cpu_count()))
-    tf_config = tf.ConfigProto(
-        allow_soft_placement=True,
-        inter_op_parallelism_threads=num_cpu,
-        intra_op_parallelism_threads=num_cpu)
+    # tf_config = tf.ConfigProto(
+    #     allow_soft_placement=True,
+    #     inter_op_parallelism_threads=num_cpu,
+    #     intra_op_parallelism_threads=num_cpu)
+    tf_config = tf.ConfigProto()
     # Prevent tensorflow from taking all the gpu memory
     tf_config.gpu_options.allow_growth = True
     if make_default:
